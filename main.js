e30dfff13bea5e59abe05e11d03b0b6eb2a1bada
Artur <devilyoushy@gmail.com>
2018-09-22 22:37:00 +0100

    well, already made it...

--- main.js
+++ main.js
@@ -1,33 +1,159 @@
 const should = {}
 
-document.getElementById('answer').innerHTML = "TAK";
+getLocation()
 
-const outputArray = [];
+const button = document.getElementById('show_button')
+button.addEventListener('click',hideshow,false);
 
-const apiAddress = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCykoFJLhcxGyaZ3oZb3YfMEPo_bWl63tU &callback=initMap'
+async function hideshow() {
+    document.getElementById('show_button').style.display = 'block'; 
+    this.style.display = 'none';
+    document.getElementById('answer').innerHTML = "TAK";
+    calculateAndRenderDirections()
+}   
 
-const getAPI = async(api, array) => {
-    // if fetched right
-    try {
-        const resp = await fetch(api)
-        const data = await resp.json()
-        array.push(data)
-        return data
-    
-    // if error occurs
-    } catch(err) {
-        console.log(err)
-        console.log("Whoops, something went wrong!")
+let map, marker;
+
+let returnPosition = {
+    lat: undefined,
+    lng: undefined
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.")
     }
 }
 
-getAPI(apiAddress, outputArray)
+function showPosition(position) {
+    returnPosition.lat = position.coords.latitude;
+    returnPosition.lng = position.coords.longitude;
+    console.log(returnPosition)
+}
 
-// const map;
+const bieszczady = {lat: 49.273, lng: 22.519}
 
-// should.initMap = () => {
-//     map = new google.maps.Map()
-// }
+function initMap() {
+    let mapCanvas = document.getElementById('map')
+    let mapOptions = {
+        center: bieszczady,
+        zoom: 12,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        overviewMapControl: false,
+        rotateControl: false,
+        styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
 
-// Google api key
-AIzaSyCykoFJLhcxGyaZ3oZb3YfMEPo_bWl63tU 
+    } 
+    
+    map = new google.maps.Map(mapCanvas, mapOptions)
+    marker = new google.maps.Marker({position: bieszczady, map: map})
+}
+
+const calculateAndRenderDirections = (origin, destination) => {
+    let directionsService = new google.maps.DirectionsService();
+    let directionsDisplay = new google.maps.DirectionsRenderer();
+        request = {
+            origin: returnPosition,
+            destination: bieszczady,
+            travelMode: 'DRIVING'
+        }
+
+    directionsDisplay.setMap(map);
+    directionsService.route(request, (result, status) => {
+        if (status == 'OK') {
+            directionsDisplay.setDirections(result)
+        }
+    })
+}
+
+
+// Google maps api key
+// AIzaSyCykoFJLhcxGyaZ3oZb3YfMEPo_bWl63tU 
+
+// Google directions api key
+// AIzaSyD9-VAQ2nuVSQMVKa-LNTGpW0Jqfx_BjUA 
